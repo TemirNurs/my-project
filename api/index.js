0907897fa44fadaa30874d85bb26334793650901
Acer <sputniknurs@gmail.com>
2021-12-26 03:40:49 +0600

    added api

---
+++ api/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const cors = require('cors')
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB connection is successful"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(express.json());
+app.use("/api/auth", authRoute );
+app.use("/api/users", userRoute );
+app.use(cors())
+
+app.listen(5000, () => {
+  console.log("backend server is running");
+});
